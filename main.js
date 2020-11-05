4461c2e88bbdfe7209942f58104940c4e29933a4
SS Jones <SSJones.code@gmail.com>
2020-11-05 15:20:04 -0700

    I changed all of the variables labeled var to be labeled const instead. I also added another button and had that button use the concat function to concatenate 4 different strings together. Concatenation itself is antiquated and should not be used. Instead, template literalls is recommended.

--- main.js
+++ main.js
@@ -5,9 +5,18 @@ const age = 34;
 console.log('My name is ' + name + ' and as of October 17th of 2020 I am ' + age + ' years old.');
 
 //concatenation using the concat function. This method does not change the existing strings, but returns a new string containing the text of the joined strings.
-function concatFunction() {
-    var str1 = 'My name is ' + name ;
-    var str2 = ' and as of October 17th of 2020 I am ' + age + ' years old.';
-    var res = str1.concat(str2);
-    document.getElementById("demo").innerHTML = res;
+function concatFunction1() {
+    const str1 = 'My name is ' + name ;
+    const str2 = ' and as of October 17th of 2020 I am ' + age + ' years old.';
+    const res = str1.concat(str2);
+    document.getElementById("concat1").innerHTML = res;
+  }
+
+  function concatFunction2() {
+    const str1 = 'Hello ';
+    const str2 = 'world!';
+    const str3 = ' I hope Luna is going to be';
+    const str4 = ' alright!';
+    const res = str1.concat(str2, str3, str4);
+    document.getElementById("concat2").innerHTML = res;
   }
