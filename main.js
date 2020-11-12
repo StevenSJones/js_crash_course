b85f9cd63c7e192912c3bc2cd669ac2e641addde
SS Jones <SSJones.code@gmail.com>
2020-11-12 12:21:22 -0700

    I attempted to change the color of the firstElementChild and was unsuccessful.

--- main.js
+++ main.js
@@ -526,5 +526,12 @@ const btn = document.querySelector(".btn");
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   //placed e in the console log so that when the click happens I can look at the event object
-  console.log(e);
+  //i then grabbed the items last el child and changed the inner HTML that said ello guvna to hi there.
+  document.querySelector('#my-form').style.background = '#red'
+  document.querySelector('body').classList.add('bg-dark');
+  document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hi there</h1>';
+  document.querySelector('.items').firstElementChild.innerHTML = ('<h1>ello guvna</h1>');
+  // document.querySelector('.items').firstElementChild.fontcolor = ('red');
 });
+
+
